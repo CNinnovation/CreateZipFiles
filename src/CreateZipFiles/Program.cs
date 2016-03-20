7a632e9af8833908e0082617dd570cdfb5052084
Christian Nagel <christian@christiannagel.com>
2016-03-20 17:19:18 +0100

    Add project files.

---
+++ src/CreateZipFiles/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+namespace CreateZipFiles
+{
+    public class Program
+    {
+        private const string SourceArchive = @"C:\Users\chris\Downloads\ProfessionalCSharp6-RC1.zip";
+        private const string TempFolder = @"c:\temp\procsharp";
+        private const string TempFolder2 = @"c:\temp\procsharp\ProfessionalCSharp6-RC1";
+        private const string RootDirectory = @"C:\Source\ProCSharpRC1\ProfessionalCSharp6";
+        private const string ResultDictionary = @"c:\temp\results";
+
+        private static Dictionary<string, string> ZipFileMapping;
+
+
+        public static void Main(string[] args)
+        {
+            InitZipFileMapping();
+            Run();
+        }
+
+        private static void Run()
+        {
+            UncompressFiles();
+            foreach (var zipFile in ZipFileMapping.Keys)
+            {
+                string zipPath = Path.Combine(ResultDictionary, zipFile);
+                string sourcePath = Path.Combine(TempFolder2, ZipFileMapping[zipFile]);
+                ZipFile.CreateFromDirectory(sourcePath, zipPath);
+                Console.WriteLine($"created {zipPath}");
+            }
+        }
+
+        private static void UncompressFiles()
+        {
+            if (!Directory.Exists(TempFolder))
+            {
+                Directory.CreateDirectory(TempFolder);
+            }
+            ZipFile.ExtractToDirectory(SourceArchive, TempFolder);
+            Console.WriteLine($"Uncompressed files to {TempFolder}");
+        }
+
+        private static void InitZipFileMapping()
+        {
+            ZipFileMapping = new Dictionary<string, string>()
+            {
+                ["01_code.zip"] = "HelloWorld",
+                ["02_code.zip"] = "CoreCSharp",
+                ["03_code.zip"] = "ObjectsAndTypes",
+                ["04_code.zip"] = "Inheritance",
+                ["05_code.zip"] = "Resources",
+                ["06_code.zip"] = "Generics",
+                ["07_code.zip"] = "Arrays",
+                ["08_code.zip"] = "OperatorsAndCasts",
+                ["09_code.zip"] = "Delegates",
+                ["09_code.zip"] = "Resources",
+                ["10_code.zip"] = "StringsAndRegularExpressions",
+                ["11_code.zip"] = "Collections",
+                ["12_code.zip"] = "SpecialCollections",
+                ["13_code.zip"] = "LINQ",
+                ["14_code.zip"] = "ErrorsAndExceptions",
+                ["15_code.zip"] = "Async",
+                ["16_code.zip"] = "ReflectionAndDynamic",
+                ["18_code.zip"] = "CompilerPlatform",
+                ["19_code.zip"] = "Testing",
+                ["20_code.zip"] = "Diagnostics",
+                ["21_code.zip"] = "Parallel",
+                ["22_code.zip"] = "Synchronization",
+                ["23_code.zip"] = "FilesAndStreams",
+                ["24_code.zip"] = "Security",
+                ["25_code.zip"] = "Networking",
+                ["26_code.zip"] = "Composition",
+                ["27_code.zip"] = "XMLandJSON",
+                ["28_code.zip"] = "Localization",
+                ["29_code.zip"] = "XAML",
+                ["30_code.zip"] = "StylesAndResources",
+                ["31_code.zip"] = "Patterns",
+                ["32_code.zip"] = "WindowsApps",
+                ["33_code.zip"] = "AdvancedWindowsApps",
+                ["34_code.zip"] = "WPF",
+                ["35_code.zip"] = "WPFDocuments",
+                ["36_code.zip"] = "DeploymentWindows",
+                ["37_code.zip"] = "ADONET",
+                ["38_code.zip"] = "EntityFramework",
+                ["39_code.zip"] = "Services",
+                ["40_code.zip"] = "ASPNET",
+                ["41_code.zip"] = "ASPNETMVC",
+                ["42_code.zip"] = "WebAPI",
+                ["43_code.zip"] = "SignalRAndWebHooks",
+                ["44_code.zip"] = "WCF",
+                ["45_code.zip"] = "DeploymentWeb"
+            };
+        }
+    }
+}
---
+++ src/CreateZipFiles/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CreateZipFiles")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CreateZipFiles")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a922f936-4248-4a3f-acff-aa102ae4f82d")]
